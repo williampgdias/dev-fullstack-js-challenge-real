ca912b533f5ea5d4c920a54689d0342b3270ca86
William Dias <smdwilliam89@gmail.com>
2022-12-30 21:21:28 +0000

    criando projeto node, instalado express, nodemon e endpoint para listar estudantes e buscar estudantes

---
+++ backend/src/database.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    nome: 'William Dias',
+    email: 'smdwilliam90@gmail.com',
+    cpf: '135.606.487-69',
+    ra: '24895',
+  },
+  {
+    nome: 'Bárbara Moreira',
+    email: 'bcarvalho89@gmail.com',
+    cpf: '345.678.132-00',
+    ra: '34906',
+  },
+  {
+    nome: 'Laura Dias',
+    email: 'lauradias@gmail.com',
+    cpf: '588.104.800-55',
+    ra: '09481',
+  },
+  {
+    nome: 'Jonathan Melo',
+    email: 'desenvolvedor.jonathan@gmail.com',
+    cpf: '939.393.169-65',
+    ra: '90366',
+  },
+];
---
+++ backend/src/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const database = require('./database');
+
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello, World');
+});
+
+app.get('/students/list', function (req, res) {
+  res.send(database);
+});
+
+app.get('/students/find/:ra', function (req, res) {
+  const studentFound = database.find(function (student) {
+    return student.ra == req.params.ra;
+  });
+  res.send(studentFound);
+});
+
+app.listen(3000);
+console.log('Server is running...');
