eb450a52dbf670350bc376463941f78a77bc2ec0
William Dias <smdwilliam89@gmail.com>
2023-01-29 18:51:41 +0000

    Iniciado projeto com react-create-app e excluido arquivos desnecessários.

---
+++ frontend/react-app/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Hello, World! {388 * 200}</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
