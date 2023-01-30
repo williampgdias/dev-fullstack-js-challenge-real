5e79e35527b6b0cd6f071f8c18a84c98d4a233a4
William Dias <smdwilliam89@gmail.com>
2023-01-30 20:58:26 +0000

    Criado o componente Navbar e separado o css dele.

--- frontend/react-app/src/App.js
+++ frontend/react-app/src/App.js
@@ -1,12 +1,42 @@
-
 import './App.css';
+
+// COMPONENTS
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1>Hello, World! {388 * 200}</h1>
-      </header>
+    <div className='main-container'>
+      <Navbar />
+
+      <section className='container'>
+        <header className='main-header'>Consulta de Alunos</header>
+        <div className='loader'></div>
+        <div className='content-page'>
+          <div className='padding-left-right-20'>
+            <div className='top-actions'>
+              <form id='formSearchStudent' className='form-search'>
+                <input type='text' name='searchInput' id='searchInput' />
+                <button>Pesquisar</button>
+              </form>
+              <a className='btn btn-dark' href='studentManager.html'>
+                Cadastrar aluno
+              </a>
+            </div>
+
+            <table id='studentList' className='table-list'>
+              <thead>
+                <tr>
+                  <th>Registro Acadêmico</th>
+                  <th>Nome</th>
+                  <th>CPF</th>
+                  <th>Ações</th>
+                </tr>
+              </thead>
+              <tbody></tbody>
+            </table>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
---
+++ frontend/react-app/src/components/Navbar/index.jsx
@@ -0,0 +1,16 @@
+import './style.css';
+
+const Navbar = () => {
+  return (
+    <nav className='main-nav'>
+      <header>Módulo Acadêmico</header>
+      <ul className='nav-links'>
+        <a className='nav-item' href='studentsList.html'>
+          <li>Alunos</li>
+        </a>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
